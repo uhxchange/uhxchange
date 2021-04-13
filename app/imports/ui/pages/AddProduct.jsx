6c04f840dedd2210412c030c329277d506e11920
chunjy <chunjy@hawaii.edu>
2021-04-13 00:04:54 -1000

    Outline of profile/add product page

---
+++ app/imports/ui/pages/AddProduct.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Contacts } from '../../api/contact/Contacts';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  firstName: String,
+  lastName: String,
+  emailAddress: String,
+  productImage: String,
+  description: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/** Renders the Page for adding a document. */
+class AddProduct extends React.Component {
+
+  // On submit, insert the data.
+  submit(data, formRef) {
+    const { firstName, lastName, emailAddress, productImage, description } = data;
+    const owner = Meteor.user().username;
+    Contacts.collection.insert({ firstName, lastName, emailAddress, productImage, description, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  render() {
+    let fRef = null;
+    return (
+      <Grid container centered>
+        <Grid.Column>
+          <Header as="h2" textAlign="center" inverted>Add Contact</Header>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+            <Segment>
+              <TextField name='firstName'/>
+              <TextField name='lastName'/>
+              <TextField name='emailAddress'/>
+              <TextField name='productImage'/>
+              <LongTextField name='description'/>
+              <SubmitField value='Submit'/>
+              <ErrorsField/>
+            </Segment>
+          </AutoForm>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default AddProduct;
