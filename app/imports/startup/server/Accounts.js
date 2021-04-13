5cf55c73f3a21eb2bae71d01597ad7da3468d43b
Hansen <hansen128@gmail.com>
2021-04-13 09:29:10 -1000

    Updated Accounts.js

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,15 +4,14 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, role, firstName, lastName, address, phoneNum) {
+function createUser(email, password, role, name, address, phoneNum) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
     email: email,
     password: password,
     profile: {
-      firstName: firstName,
-      lastName: lastName,
+      name: name,
       address: address,
       phoneNum: phoneNum,
     },
@@ -27,7 +26,7 @@ function createUser(email, password, role, firstName, lastName, address, phoneNu
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role, firstName, lastName, address, phoneNum }) => createUser(email, password, role, firstName, lastName, address, phoneNum));
+    Meteor.settings.defaultAccounts.map(({ email, password, role, name, address, phoneNum }) => createUser(email, password, role, name, address, phoneNum));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
