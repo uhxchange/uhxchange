d89643fdf25d6c3bb5b3f269a98622089dd86129
Hansen <hansen128@gmail.com>
2021-04-13 01:04:38 -1000

    Updated Accounts.js to have additional data in defaultAccounts

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,12 +4,18 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, password, role) {
+function createUser(email, password, role, firstName, lastName, address, phoneNum) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
     email: email,
     password: password,
+    profile: {
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      phoneNum: phoneNum,
+    },
   });
   if (role === 'admin') {
     Roles.createRole(role, { unlessExists: true });
@@ -21,7 +27,7 @@ function createUser(email, password, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
+    Meteor.settings.defaultAccounts.map(({ email, password, role, firstName, lastName, address, phoneNum }) => createUser(email, password, role, firstName, lastName, address, phoneNum));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
