c25c0a0700e6b5aeef273040f5b515b3cd315919
tamtn2 <77471621+tamtn2@users.noreply.github.com>
2021-04-14 16:42:59 -1000

    Change landing page and update "aesthetic"

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
+      <Grid verticalAlign='middle' textAlign='center' container>
+        <Grid.Row>
+          <Header as='h1'> Welcome to UHXchange </Header>
+        </Grid.Row>
+        <Image src='https://www.moneycrashers.com/wp-content/uploads/2016/11/flea-market-shopping-tips-1068x713.jpg' size='small' floated='left'/>
+        <p>
+          Welcome to UHXchange! This is a web application solely for the University of Hawaii community in which
+          users can sell goods. All you have to do is sign up if you do not have an account with us, or login
+          with an account you already own. Then, simply go to the marketplace, search or browse any items on
+          display!
+        </p>
       </Grid>
     );
   }
