6f8800292d26c2eb8484a590763ce3098430d095
tamtn2 <77471621+tamtn2@users.noreply.github.com>
2021-04-20 16:51:02 -1000

    Landing page updated

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Grid, Header, Image } from 'semantic-ui-react';
+import { Button, Grid, Header } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid verticalAlign='middle' textAlign='center' container>
-        <Grid.Row>
-          <Header as='h1'> Welcome to UHXchange </Header>
-        </Grid.Row>
-        <Image src='https://www.moneycrashers.com/wp-content/uploads/2016/11/flea-market-shopping-tips-1068x713.jpg' size='small' floated='left'/>
-        <p>
-          Welcome to UHXchange! This is a web application solely for the University of Hawaii community in which
-          users can sell goods. All you have to do is sign up if you do not have an account with us, or login
-          with an account you already own. Then, simply go to the marketplace, search or browse any items on
-          display!
-        </p>
-      </Grid>
+      <div className='landing-background-page'>
+        <Grid verticalAlign='middle' textAlign='center' container>
+          <Grid.Row>
+            <Header as='h1' inverted> Welcome to UHXchange </Header>
+          </Grid.Row>
+          <Grid.Row>
+            <Header as='h3' inverted>
+              Welcome to UHXchange! This is a web application solely for the University of Hawaii community in which
+              users can sell goods. All you have to do is sign up if you do not have an account with us, or login
+              with an account you already own.
+            </Header>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <Button as={NavLink} exact to="/signin">Sign In</Button>
+            </Grid.Column>
+            <Grid.Column width={2}>
+              <Button as={NavLink} exact to="/signup">Sign Up</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     );
   }
 }
