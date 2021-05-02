b60046a15679a620279d6f5173b0e2124fefde19
chunjy <chunjy@hawaii.edu>
2021-05-02 16:30:20 +0900

    Updated Footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -11,7 +11,7 @@ export default class BottomMenu extends React.Component {
               <List.Item id='footer-title'>INFORMATION</List.Item>
               <hr/>
               <List.Item><a href={'/#/home'}>About Us</a></List.Item>
-              <List.Item><a href={'https://uhxchange.github.io/'}>Contact Us</a></List.Item>
+              <List.Item><a href={'https://uhxchange.github.io/#team'}>Contact Us</a></List.Item>
               <List.Item><a href={'/#/review'}>Community Feedback</a></List.Item>
             </List>
           </Grid.Column>
