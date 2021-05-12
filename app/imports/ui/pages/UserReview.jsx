eef4efdf39711275aed2f8f76710595608c968cd
chunjy <chunjy@hawaii.edu>
2021-05-12 23:07:09 +0900

    ESLint check

--- app/imports/ui/pages/UserReview.jsx
+++ app/imports/ui/pages/UserReview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Header, Feed, Loader } from 'semantic-ui-react';
+import { Grid, Feed, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import Review from '../components/Review';
